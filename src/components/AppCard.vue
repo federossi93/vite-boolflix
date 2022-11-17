b82342333eeb86891341612e4ba3090dbba62935
federossi93 <rossifederico0993@gmail.com>
2022-11-17 16:36:17 +0100

    add vote

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,35 +1,36 @@
 <script>
-import {state} from '../state.js'
+import { state } from '../state.js'
 
 
-export default{
-    name:'AppCard',
-    data(){
-        return{
-            state,
-        }
+export default {
+  name: 'AppCard',
+  data() {
+    return {
+      state,
     }
+  }
 }
 
 </script>
 
 <template>
 
-<div class="container" v-if="state.resultSerch">
-      <div class="row row-cols-4">
-        <div class="col my-4" v-for="movie in state.resultSerch.results">
-          <div class="card">
-            <img :src="state.image + movie.poster_path" alt="">
-            <div></div>
+  <div class="container" v-if="state.resultSerch">
+    <div class="row row-cols-4">
+      <div class="col my-4" v-for="movie in state.resultSerch.results">
+        <div class="card">
+          <img :src="state.image + movie.poster_path" alt="">
+          <div class="d-none">
             <div>{{ movie.title }}</div>
             <div>{{ movie.original_title }}</div>
             <div v-if="movie.original_language === 'en'">Lenguage: <span class="text-primary">En</span></div>
             <div v-if="movie.original_language === 'ja'">Lenguage: <span class="text-secondary">Ja</span></div>
             <div v-if="movie.original_language === 'hu'">Lenguage: <span class="text-danger">Hu</span></div>
-            <div>{{ movie.vote_average }}</div>
+            <div>{{ Math.round(movie.vote_average / 2) }}</div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
 </template>
