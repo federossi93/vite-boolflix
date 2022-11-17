64ab79df2db0a6071a9698d6fd95242b950eaed2
federossi93 <rossifederico0993@gmail.com>
2022-11-17 08:37:57 +0100

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import AppHeader from "./components/AppHeader.vue";
+//import { store } from "./store.js";
 
 
 export default {
@@ -9,12 +10,29 @@ export default {
   components: {
     AppHeader,
   },
+
   data() {
     return {
-      
+      //store
+      API_URL: "https://api.themoviedb.org/3/search/movie?api_key=aa06742d67521bd0cab47843c3bc5f06&language=en-US&query=Matrix&page=1&include_adult=false",
+      series: null,
     };
   },
-  
+
+  methods: {
+    callApi(url){
+      axios.get(url)
+      .then(response =>{
+        console.log(response);
+        this.series = response.data.results
+      })
+    }
+    
+  },
+
+  mounted() {
+    this.callApi(this.API_URL)
+  }
 }
 
 
@@ -25,7 +43,20 @@ export default {
   <AppHeader />
 
   <main>
-    
+
+    <div class="container">
+      <div class="row row-cols-4">
+        <div class="col" v-for="serie in series">
+          <div class="serie card my-3">
+            <div>Tilte: {{serie.title}}</div>
+            <div>Original Title: {{serie.original_title}}</div>
+            <div>Lenguage: {{serie.original_language}}</div>
+            <div>Vote: {{serie.vote_average}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </main>
 
 </template>
